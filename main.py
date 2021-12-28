9ede8164e704f5925ef9b369adf25e1d21acb034
zhanghongzhi <960112559@qq.com>
2021-12-28 18:09:18 +0800

    1

--- main.py
+++ main.py
@@ -134,15 +134,22 @@ def find_and_paste(cookie):
     return jd_cookie
 
 def put_cookie(cookie):
-    req_headers = {'Content-Type': 'application/json'}
-    req_body = json.dumps({"cookie": cookie})
-    response = requests.post("http://127.0.0.1:8090/jd/cookie/put", data=req_body, headers=req_headers)
-    print(response.text)
-    res_body = json.loads(response.text)
-    if res_body['status']:
-        print("提交 cookie 成功")
+    try:
+        req_headers = {'Content-Type': 'application/json'}
+        req_body = json.dumps({"cookie": cookie})
+        response = requests.post("http://127.0.0.1:8090/jd/cookie/put", data=req_body, headers=req_headers)
+    except requests.exceptions.ConnectionError:
+        print("提交Cookie失败，服务器连接失败。")
     else:
-        print("提交 cookie 失败，错误信息：" + res_body['message'])
+        if response.status_code == 200:
+            print(response.text)
+            res_body = json.loads(response.text)
+            if res_body['status']:
+                print("提交Cookie成功")
+            else:
+                print("提交Cookie失败，错误信息：" + res_body['message'])
+        else:
+            print("提交Cookie失败，响应码：" + response.status_code)
 
 def main():
     print('请在弹出的网页中登录账号。')
